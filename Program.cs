c154ffb2ad31efac3bb76f916bd575563e7318da
Vashtany <vashcevich.tania@gmail.com>
2023-10-24 20:01:21 +0300

    Решение задачи

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿string[] array = {"Hello", "2", "world", ":-)"};
+
+string[] newArray = new string[0];
+        
+int i = 0;
+
+while (i < array.Length)
+    {
+        if (array[i].Length <= 3)
+            {
+                Array.Resize(ref newArray, newArray.Length + 1);
+                
+                newArray[newArray.Length - 1] = array[i];
+            }
+            
+        i++;
+    }
+        
+Console.WriteLine("Новый массив:");
+foreach (string s in newArray)
+{
+    Console.WriteLine(s);
+}
